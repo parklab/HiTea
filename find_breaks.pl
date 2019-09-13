cdcafd96821c04a0cfc5f08714386d9f026b5292
dhawalsjain <dhawal.sjain@gmail.com>
2019-09-12 22:44:18 -0400

    Update find_breaks.pl

--- find_breaks.pl
+++ find_breaks.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
-# Author:
-# Dhawal Jain, Dept of Biomedical Informatics, Harvard
+# HiTEA
 # This script extacts break positions
 
 use warnings FATAL => "all";
@@ -17,24 +16,26 @@ use Utilities;
 
 BEGIN { our $start_run = time(); }
 
-my $sam = "";
 my $bam = "";
 my $wd ="";
 my $hd ="";
+my $remap ="F";
+my $TE_element="";
 my $outprefix="project";
 my $help=0;
 Getopt::Long::GetOptions(
-  'sam=s'            => \$sam,
   'bam=s'            => \$bam,
   'outprefix=s'      => \$outprefix,
   'wd:s'             => \$wd,
   'hd:s'             => \$hd,
+  'remap:s'          => \$remap,
+  'te:s'          => \$TE_element,
   'help'             => \$help,
   'h'                => \$help,
   ) or die "Incorrect input! Use -h for usage.\n";
 
 if ($help) {
-  print "\nUsage: perl find_breaks.pl -bam [FILE_PATH] -outprefix [STRING] -wd [DIR_PATH] -hd [FILE_PATH] \n\n";
+  print "\nUsage: perl find_breaks.pl -bam [FILE_PATH] -outprefix [STRING] -wd [DIR_PATH] -hd [FILE_PATH] -remap [T/F] -te [STRING] \n\n";
   print "This script generates a bam file (can be used for visualization) and breakpoint list file from mapped sam/bam\n\n";
   print "Options:\n";
   print "required**:\n";
@@ -43,6 +44,8 @@ if ($help) {
   print "optional:\n";
   print "   -wd              Working directory [default: ~]\n";
   print "   -outprefix       Outputprefix for generating the output files \n";
+  print "   -remap           Whether to remap the unmapped clipped sequences to PolyMoprhic sequence assembly? (default=F)\n";
+  print "   -te              TE for which polymoprhic rempping is being performed\n";
   print "   -help|-h         Display usage information.\n\n\n";
   print "Default outputs:\n";
   print "   (outprefix).clusters.bed\n";
@@ -87,25 +90,33 @@ my $run_time=0;
 #--------------------------------------------------------------------------------------------------------------
 $watch_run = time();
 $run_time = $watch_run - our $start_run;
-print "\n[extract_clusters=Start_Time]:\t $run_time seconds\n";
+print "[extract_clusters=Start_Time]:\t $run_time seconds\n";
 $outprefix =~ s/\?//g;
 $outprefix =~ s/\#//g;
 $outprefix =~ s/\'//g;
 $outprefix =~ s/\"//g;
 $outprefix =~ s/\s*//g;
-
+if($remap eq "T" and $TE_element eq ""){
+  print " TE element not designated for remapping. Exiting! \n";
+  exit 1;
+}
 
 my $out1 = $outprefix.".temp.clusters.bed";
-open O1, ">> $out1" or die "can't create $out1";
 my $out2 = $outprefix.".temp.RAM.bam";
-open O2, "| samtools view -@ 4 -bS - >> $out2" or die "can't create $out2";
-
+my $out3 =  $outprefix.".temp.remap.fq";
+if($TE_element ne "" and $remap eq "T"){
+  $out2 = $outprefix.".".$TE_element.".temp.RAM.bam";
+  $out3 =  $outprefix.".".$TE_element.".temp.remap.fq.gz";
+  open O3, "|gzip -c - > $out3" or die "can't create $out3";
+}
+open O1, ">> $out1" or die "can't create $out1";
+open O2, "| samtools view -bS - > $out2" or die "can't create $out2";
 if($hd ne ""){
- open(F, "$hd") or die $!;
- my @header = <F>;
- print O2 @header;
- close F;
- undef(@header);
+  open(F, "$hd") or die $!;
+  my @header = <F>;
+  print O2 @header;
+  close F;
+  undef(@header);
 }
 
 ## Get cluster coordinate from Repeat-mapped clipped reads
@@ -145,7 +156,7 @@ while( my( $i, $j ) = each %hqual ) {
   }
 }
 close(O1);
-
+close(O3) if($remap eq "T");
 print " Total number of clip read positions in the file: $cls\n";
 print " Total badlines in the file: $badline\n";
 $watch_run = time();
@@ -159,66 +170,95 @@ exit 0;
 sub get_breakpoints {
  my ($file) = shift;
  my $i=0;
- 
+ my $oid ="-";
+ my %res;
+
  while(<$file>){
     next if(/^(\#)/);
     next if(/^(\@)/); 
     chomp;
     s/\r//;  
     next if($_ eq "");
-    my @temp = split(/\t+/);
-    next if($temp[1] >=256);  
-    next if($temp[1] ==4 and $temp[0]!~ /DE,TP/);
-
-    my ($score) = $_ =~ /\tAS:i:(\d*)/;
-    my $teclust = "-";
-    my $strand = "*";
-    my ($mismatch) = "NA";
-    my $read_strand="+";
+    my @temp = split(/\t/);    
+    if($temp[1] ==4 and $temp[0]!~ /DE,TP/){
+      next;
+    }
+    if($temp[1]==4 and $temp[0]=~ m/DE,TP/ and $remap eq "T"){
+      print O3 "@","$temp[0]\n$temp[9]\n+\n$temp[10]\n";
+      next;
+    }elsif($temp[1]==4 and $temp[0]=~ m/DE,TP/ and $remap eq "F"){
+      my @sam = split("\x{019}",$temp[0]);
+      my $end = Utilities::get_read_mapping_span($sam[3],$sam[5]);
+      $sam[-1] .= ";TE=*;mm=0;sc=0;str=*;end=".$end.";";
+      print O2 join("\t",@sam),"\n";
+      next;
+    }
     
-    my @sam = split("\x{019}",$temp[0]);
-    $read_strand="-" if($sam[1] & 0x10);
-  
-    ## Add map-start on concensus
-    if($temp[1] !=4){
-      undef($teclust);
-      $strand = "+";
-      $strand = "-" if($temp[1] & 0x10);
-      if($read_strand eq "-" and $strand eq "+"){
-        $teclust .= $temp[2].":".$temp[3].";";   ## adjust for clip ori
-      }else{
-        $teclust .= $temp[2].":".Utilities::get_clip_coord($temp[3],$temp[5]).";";   
-      }      
-      if($_=~ /XA:Z:(\S*);$/){
-        my @altmap = split(";",$1);
-        foreach my $a (@altmap){
-           my @al = split(",",$a);
-           if($al[0] ne $temp[2]){
-             my $sstr = "+";
-             if($al[1] =~ m/-\d*/){ $sstr = "-";}
-             $al[1]=~ s/[+-]//g;
-             if($read_strand eq "-" and $sstr eq "+"){
-                $teclust .= $al[0].":".$al[1].";"; 
-              }else{
-                $teclust .= $al[0].":".Utilities::get_clip_coord($al[1],$al[2]).";"; 
-             }
-           }
-        }  
+    my ($mismatch) = "NA";
+    my $strand = "*";
+    my ($score) = $_ =~ /\tAS:i:(\d*)/;
+    if($_=~ /\tNM:i:(\d*)/){ $mismatch=$1;}
+    $strand = "+";
+    $strand = "-" if($temp[1] & 0x10);
+
+    if($oid eq "-" or ($oid ne "-" and $oid eq $temp[0])){
+      $oid =$temp[0] if($oid eq "-");
+      if(!exists $res{$temp[2]} or $res{$temp[2]}{score} < $score){
+         $res{$temp[2]}{score} = $score;
+         $res{$temp[2]}{mm} = $mismatch;
+         $res{$temp[2]}{str} = $strand;
+         $res{$temp[2]}{start} = $temp[3];
+         $res{$temp[2]}{cigar} = $temp[5];
       }
     }
-    $teclust =~ s/;$//;
-    if($_=~ /\tNM:i:(\d*)/){ $mismatch=$1;}
-    $sam[-1] .= ";TE=".$temp[2].";mm=".$mismatch.";sc=".$score.";str=".$strand.";cl=".$teclust;
-    my $id = $sam[2].$sam[3].$sam[5].$sam[6].$sam[7];
-    my $end = Utilities::get_read_mapping_span($sam[3],$sam[5]);
-    $sam[-1] .= ";end=".$end;
-    
-    my ($cliploc) = $sam[-1] =~ m/;clip=(\d+);/;
-    my ($sidedness) = $sam[-1] =~ m/;side=(\S+?);/;     
-    if($temp[0]=~ m/DE,TP/ and $temp[2] ne "*" and $sam[4]>0){ ## only take locations with some TE mapping for cluster generation, that aren;t multimapper
-       $hqual{$sam[2]."\t".$cliploc."\t".$sidedness}.=$id."|".$score."|c:".$cliploc." ";
-    }     
-    print O2 join("\t",@sam),"\n";  
+
+    if($oid ne "-" and $oid ne $temp[0]){
+      ## write
+      my @sam = split("\x{019}",$oid);
+      my $end = Utilities::get_read_mapping_span($sam[3],$sam[5]);
+      my $read_strand="+";
+      $read_strand="-" if($sam[1] & 0x10);       
+      my $te;
+      my $sc;
+      my $mm;
+      my $str; 
+      my $maxscore;
+      foreach my $tel (keys %res){
+        if($read_strand eq "-" and $res{$tel}{str} eq "+"){
+          $te .= $tel.":".$res{$tel}{start}.",";   ## adjust for clip ori
+        }else{
+          $te .= $tel.":".Utilities::get_clip_coord($res{$tel}{start},$res{$tel}{cigar}).",";   
+        }
+        $sc .= $res{$tel}{score}.",";
+        if(!defined $maxscore or $maxscore<$res{$tel}{score}){
+          $maxscore = $res{$tel}{score};
+        }
+        $mm .= $res{$tel}{mm}.",";
+        $str .= $res{$tel}{str}.",";
+      }
+      $str =~ s/,$//;
+      $mm =~ s/,$//;
+      $te =~ s/,$//;
+      $sc =~ s/,$//;
+      $sam[-1] .= ";TE=".$te.";mm=".$mm.";sc=".$sc.";str=".$str.";end=".$end.";";
+      my ($cliploc) = $sam[-1] =~ m/;clip=(\d+);/;
+      my ($sidedness) = $sam[-1] =~ m/;side=(\S+?);/;     
+      my $id = $sam[2].$sam[3].$sam[5].$sam[6].$sam[7];
+      print O2 join("\t",@sam),"\n";
+      
+      if($sam[-1] =~ m/DE,TP/ and $sam[4]>0){ ## only take locations with some TE mapping for cluster generation, that aren;t multimapper
+         $hqual{$sam[2]."\t".$cliploc."\t".$sidedness}.=$id."|".$maxscore."|c:".$cliploc." ";
+      }
+      
+      ## reset
+      undef %res;
+      $oid = $temp[0];  
+      $res{$temp[2]}{score} = $score;
+      $res{$temp[2]}{mm} = $mismatch;
+      $res{$temp[2]}{str} = $strand;
+      $res{$temp[2]}{start} = $temp[3];
+      $res{$temp[2]}{cigar} = $temp[5];
+    }
  }
 }
 ###----------------------------------------------- END OF THE SCRIPT ---------------------------------------------------
